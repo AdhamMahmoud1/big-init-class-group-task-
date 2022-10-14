57b686d768972d814f19beab21295efb42ce83ae
AdhamMahmoud1 <101528034+AdhamMahmoud1@users.noreply.github.com>
2022-10-14 13:22:30 +0200

    Update BigInt.h

--- BigInt.h
+++ BigInt.h
@@ -11,6 +11,11 @@ class BigInt {
         BigInt(string);
         BigInt(long long);
         BigInt& operator+ (BigInt num1);
+        bool operator> (BigInt anotherDec);
+        bool operator== (BigInt anotherDec);
         BigInt& operator= (BigInt anotherDec);
+        int size();
+        int sign();
+        friend ostream& operator << (ostream& out, BigInt b);
 };
 #endif
