90a0a7f711ebe58b62ce2fd8e4ca010e7938b7a3
FadyKamal <popfadykamal151617@gmail.com>
2022-10-15 19:41:59 +0200

    added some comments

--- BigInt.h
+++ BigInt.h
@@ -22,6 +22,8 @@ class BigDecimalInt {
         BigDecimalInt operator+ (BigDecimalInt num);
         BigDecimalInt& operator= (BigDecimalInt anotherDec);
         friend ostream& operator << (ostream& out, BigDecimalInt b);
+        friend bool operator<(const BigDecimalInt &a, const BigDecimalInt &b);
+
         bool operator> (BigDecimalInt anotherDec);
         bool operator== (BigDecimalInt anotherDec);
 
