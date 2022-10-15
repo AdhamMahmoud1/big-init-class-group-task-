2f62d2e274e32d4c2bd61bf1b4994748dbd33398
FadyKamal <popfadykamal151617@gmail.com>
2022-10-15 21:27:04 +0200

    Teasted < in main file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include "BigInt.h"
+#include "BigInt.cpp"
 using namespace std;
 
 
 // Drive code
 int main()
-{
+{   
+    BigDecimalInt first("454454"), second("45445");
+    cin >> first >> second;
+
+
+    if (first > second)
+    {
+        cout << first << " > " << second;
+    }
+    else
+    {
+        cout << first << " < " << second;
+    }
     
     return 0;
 }
